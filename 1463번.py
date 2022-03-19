0e46869686fd4a308e0311ff8a5cdce8fd4db3d6
star2usemap <star2usemap@gmail.com>
2022-03-20 01:11:31 +0900

    no message

--- 1463번.py
+++ 1463번.py
@@ -4,9 +4,9 @@ def solution(n):
     for i in range(2, n + 1):
         dp[i] = dp[i - 1] + 1
         if i % 3 == 0:
-            dp[i] = min(dp[i - 1], dp[i // 3]) + 1
-        elif i % 2 == 0:
-            dp[i] = min(dp[i - 1], dp[i // 2]) + 1
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
 
     return dp[n]
 n = int(input())
