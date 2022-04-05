00982019104ee81f262b85b44919b08502d1a9af
star2usemap <star2usemap@gmail.com>
2022-04-06 00:23:58 +0900

    no message

--- 구현/17143번.py
+++ 구현/17143번.py
@@ -1,3 +1,4 @@
+import sys
 class Shark:
     def __init__(self, row, column, speed, size, direction):
         self.row = row
@@ -16,6 +17,7 @@ dy = [-1, 1, 0, 0]
 dx = [0, 0, 1, -1]
 
 def solution():
+    input = sys.stdin.readline
     r, c, m = map(int, input().split())
     sharks = []
     board = [[None for i in range(c)] for _ in range(r)]
@@ -25,7 +27,6 @@ def solution():
         shark = Shark(row - 1, column - 1, speed, size, direction - 1)
         board[shark.row][shark.column] = shark
         sharks.append(shark)
-
 
     ret = 0
     for j in range(c):
@@ -93,57 +94,3 @@ def solution():
     return ret;
 
 print(solution())
-
-'''
-
-            k = 1
-            while k <= speed:
-                n_row = shark.row + dy[shark.direction]
-                n_column = shark.column + dx[shark.direction]
-
-                if not (0 <= n_row < r and 0 <= n_column < c): #다음 위치가 벽일 때
-                    shark.reverse_direction()
-                    n_row = shark.row + dy[shark.direction]
-                    n_column = shark.column + dx[shark.direction]
-                shark.row = n_row
-                shark.column = n_column
-                k += 1
-
-            '''
-
-'''
-def test(r, c, row, column, speed, direction):
-    # 스피드만큼 반복
-    n_row = row + (dy[direction] * speed)
-    n_column = column + (dx[direction] * speed)
-
-    if n_row < 0:
-        n_row = -((-n_row) % ((r - 1) * 2))
-    else:
-        n_row = n_row % ((r - 1) * 2)
-
-    if n_column < 0:
-        n_column = -((-n_column) % ((c - 1) * 2))
-    else:
-        n_column = n_column % ((c - 1) * 2)
-
-    while not (0 <= n_row < r and 0 <= n_column < c):
-        if n_row < 0:
-            direction += 1
-            n_row = n_row * -1
-        elif n_row >= r:
-            direction -= 1
-            n_row = (r - 1) - (n_row - (r - 1))
-        elif n_column < 0:
-            direction -= 1
-            n_column = n_column * -1
-        elif n_column >= c:
-            direction += 1
-            n_column = (c - 1) - (n_column - (c - 1))
-    print(n_row, n_column)
-'''
-
-
-
-
-
