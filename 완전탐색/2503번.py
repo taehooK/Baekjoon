dd3b0a5057c760b0ea4cfdf0b95f9804514bf038
star2usemap <star2usemap@gmail.com>
2022-04-20 12:59:41 +0900

    no message

---
+++ 완전탐색/2503번.py
@@ -0,0 +1,42 @@
+def is_correct():
+    for query in queries:
+        strike = 0
+        ball = 0
+        for i in range(len(array)):
+            index = query[0].find(array[i])
+            if index >= 0:
+                if i == index:
+                    strike += 1
+                else:
+                    ball += 1
+        if strike != int(query[1]) or ball != int(query[2]):
+            return False
+    return True
+
+
+def recursive(index):
+    global ret_count
+    if index >= len(array):
+        if is_correct():
+            ret_count += 1
+        return
+
+    for i in range(1, 10):
+        array[index] = str(i)
+        j = index - 1
+        while j >= 0 and array[index] != array[j]:
+            j -= 1
+
+        if j < 0:
+            recursive(index + 1)
+
+n = int(input())
+ret_count = 0
+array = ['0'] * 3
+queries = [tuple(map(str, input().split())) for i in range(n)]
+recursive(0)
+print(ret_count)
+
+
+
+
